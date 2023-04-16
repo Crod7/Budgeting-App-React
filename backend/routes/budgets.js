25968ddc86c03964cbdd3684e043f56bd22b5721
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-04-16 08:37:36 -0700

    Added middleware functionality that requires all users to authenticate their login tokens before accessing any routes in the program. Login/ Signup are now required to use app.

--- backend/routes/budgets.js
+++ backend/routes/budgets.js
@@ -11,23 +11,23 @@ const {                                         // We are importing many things 
 
 } = require('../controllers/budgetController')  // This is where we are importing these functions from
 
+const requireAuth = require('../middleware/requireAuth')    // We import the middleware that checks auth before
+                                                            // allowing user to use route
+
 
 // This creates an instance of the Router
 const router = express.Router()
 
+router.use(requireAuth)                         // fires middleware function before router
+                                                // authorization needed for all routes
+
 
 // A function will run once a get request comes in defined here
-// GET all workouts
-router.get('/', getAllBudgets)
-// GET a single workout
-router.get('/:id', getBudget)
-// POST a new workout
-router.post('/', createBudget)
-
-// DELETE a new workout
-router.delete('/:id', deleteBudget)
-// UPDATE a new workout
-router.patch('/:id', updateBudget)
+router.get('/', getAllBudgets)                  // GET all transactions
+router.get('/:id', getBudget)                   // GET a single transaction
+router.post('/', createBudget)                  // POST a new transaction
+router.delete('/:id', deleteBudget)             // DELETE a new transaction
+router.patch('/:id', updateBudget)              // UPDATE a new transaction
 
 // Export to rest of program
 module.exports = router
