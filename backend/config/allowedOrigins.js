0953fc074f72d99df9701429ee4d54461b0b24d4
Carlos Rodriguez <crodriguezyt10@gmail.com>
2023-06-20 18:14:50 -0700

    Removed the origin requirement from the cors.

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -4,10 +4,12 @@
 // AFTER DEVELOPMENT IS COMPLETE
 
 const allowedOrigins= [
-    'htttp://www.yoursite.com', 
+    'https://budgetingreactapp.onrender.com', 
     'http://127.0.0.1:5500',
     'http://localhost:5500/', 
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'localhost:3000',
+    'localhost:3000/'
 ]
 
 module.exports = allowedOrigins
